9ae02497d50b91244e1592466f5b19e786e489fb
pavlo.zaporozkyi <pavlo.zaporozkyi@mev.com>
2023-02-12 20:27:22 +0200

    Init

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>
+          env: {process.env.REACT_APP_ENV}
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
